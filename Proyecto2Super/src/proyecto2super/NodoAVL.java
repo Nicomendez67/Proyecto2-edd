cd5cb505c9019bdb897238ec02db3f87a5c0f70b
tony-2006 <antonio.yibirin@correo.unimet.edu.ve>
2025-11-23 07:11:48 -0400

    Clase Nodo AVL y metodos de AVLPalabras clave

--- Proyecto2Super/src/proyecto2super/NodoAVL.java
+++ Proyecto2Super/src/proyecto2super/NodoAVL.java
@@ -10,4 +10,59 @@ package proyecto2super;
  */
 public class NodoAVL {
     
+    private String palabra;
+    private ListaEnlazada<Investigacion> investigaciones;
+    private int altura;
+    private NodoAVL izquierdo;
+    private NodoAVL derecho;
+
+        
+    NodoAVL(String palabra, Investigacion inv) {
+        this.palabra = palabra;
+        this.investigaciones = new ListaEnlazada<>();
+        this.investigaciones.agregar(inv);
+        this.altura = 1;
+    }
+
+    public String getPalabra() {
+        return palabra;
+    }
+
+    public void setPalabra(String palabra) {
+        this.palabra = palabra;
+    }
+
+    public ListaEnlazada<Investigacion> getInvestigaciones() {
+        return investigaciones;
+    }
+
+    public void setInvestigaciones(ListaEnlazada<Investigacion> investigaciones) {
+        this.investigaciones = investigaciones;
+    }
+
+    public int getAltura() {
+        return altura;
+    }
+
+    public void setAltura(int altura) {
+        this.altura = altura;
+    }
+
+    public NodoAVL getIzquierdo() {
+        return izquierdo;
+    }
+
+    public void setIzquierdo(NodoAVL izquierdo) {
+        this.izquierdo = izquierdo;
+    }
+
+    public NodoAVL getDerecho() {
+        return derecho;
+    }
+
+    public void setDerecho(NodoAVL derecho) {
+        this.derecho = derecho;
+    }
+    
+    
 }
