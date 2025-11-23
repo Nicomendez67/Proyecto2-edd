036deb05524d4d727e06f884ff36566c8b3c3c00
tony-2006 <antonio.yibirin@correo.unimet.edu.ve>
2025-11-23 00:21:30 -0400

    Clase Nodo de la lista

---
+++ Proyecto2Super/src/proyecto2super/NodoLista.java
@@ -0,0 +1,37 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package proyecto2super;
+
+/**
+ *
+ * @author Nicolas Mendez, Antonio Yibirin
+ */
+public class NodoLista<T> {
+    private T Dato;  
+    private NodoLista<T> Siguiente;
+
+    public NodoLista(T Dato, NodoLista<T> Siguiente) {
+        this.Dato = Dato;
+        this.Siguiente = Siguiente;
+    }
+
+    public T getDato() {
+        return Dato;
+    }
+
+    public void setDato(T Dato) {
+        this.Dato = Dato;
+    }
+
+    public NodoLista<T> getSiguiente() {
+        return Siguiente;
+    }
+
+    public void setSiguiente(NodoLista<T> Siguiente) {
+        this.Siguiente = Siguiente;
+    }
+    
+    
+}
