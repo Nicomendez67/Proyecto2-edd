c4b93442d0597a62db021e60c47d97f348743365
tony-2006 <antonio.yibirin@correo.unimet.edu.ve>
2025-11-23 00:54:17 -0400

    AVL palabras clave, arbol basico

---
+++ Proyecto2Super/src/proyecto2super/AVLPalabrasClave.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package proyecto2super;
+
+/**
+ *
+ * @author Nicolas Mendez, Antonio Yibirin
+ */
+public class AVLPalabrasClave {
+    private NodoAVL raiz;
+
+    public AVLPalabrasClave(NodoAVL raiz) {
+        this.raiz = raiz;
+    }
+
+    public NodoAVL getRaiz() {
+        return raiz;
+    }
+
+    public void setRaiz(NodoAVL raiz) {
+        this.raiz = raiz;
+    }
+    
+    
+}
