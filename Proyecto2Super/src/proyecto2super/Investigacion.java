739190203165c692b6607acec4df095f55639422
tony-2006 <antonio.yibirin@correo.unimet.edu.ve>
2025-11-23 00:10:45 -0400

    Clase investigacion

---
+++ Proyecto2Super/src/proyecto2super/Investigacion.java
@@ -0,0 +1,58 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package proyecto2super;
+
+/**
+ *
+ * @author Nicolas Mendez, Antonio Yibirin
+ */
+public class Investigacion {
+    
+    String titulo;
+    String[] autores;
+    String cuerpo;
+    String[] palabrasClaves;
+
+    public Investigacion(String titulo, String[] autores, String cuerpo, String[] palabrasClaves) {
+        this.titulo = titulo;
+        this.autores = autores;
+        this.cuerpo = cuerpo;
+        this.palabrasClaves = palabrasClaves;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String[] getAutores() {
+        return autores;
+    }
+
+    public void setAutores(String[] autores) {
+        this.autores = autores;
+    }
+
+    public String getCuerpo() {
+        return cuerpo;
+    }
+
+    public void setCuerpo(String cuerpo) {
+        this.cuerpo = cuerpo;
+    }
+
+    public String[] getPalabrasClaves() {
+        return palabrasClaves;
+    }
+
+    public void setPalabrasClaves(String[] palabrasClaves) {
+        this.palabrasClaves = palabrasClaves;
+    }
+    
+    
+}
